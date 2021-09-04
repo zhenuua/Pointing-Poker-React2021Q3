8bc30024bdc5165902153414bc9ee58b50b27243
zhenya zakharenko <zhenuua@gmail.com>
2021-09-04 21:29:08 +0300

    init: start pointing poker project

---
+++ pointing-poker/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import MainPage from './pages/main/MainPage';
+
+const App: React.FC = (): JSX.Element => (
+  <div className="App">
+    <h2>Hello </h2>
+    <MainPage />
+  </div>
+);
+
+export default App;
---
+++ pointing-poker/src/pages/main/MainPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import style from './main.module.scss';
+
+const MainPage = (): JSX.Element => (
+  <section className={style.main}>
+    <h2 className={style.header}>MainPage</h2>
+  </section>
+);
+export default MainPage;
