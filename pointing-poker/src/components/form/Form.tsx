e976a1341518dfa21ac85c49e40aa1893181001e
Maxim Kalevich <maxkalevich@gmail.com>
2021-09-07 22:36:42 +0300

    test: task form

---
+++ pointing-poker/src/components/form/Form.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+import style from './Form.module.scss';
+
+const Form: React.FC = (): JSX.Element => {
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [jobPosition, setJobPosition] = useState<string>('');
+
+  return (
+    <form>
+      <label htmlFor="firstName">
+        <span>Your first name:</span>
+        <input
+          className={style.firstName}
+          type="text"
+          id="firstName"
+          name="firstName"
+          value={firstName}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setFirstName(event.target.value)
+          }
+        />
+      </label>
+      <label htmlFor="lastName">
+        <span>Your last name:</span>
+        <input
+          className={style.lastName}
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={lastName}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setLastName(event.target.value)
+          }
+        />
+      </label>
+      <label htmlFor="jobPosition">
+        <span>Your job position:</span>
+        <input
+          className={style.jobPosition}
+          type="text"
+          id="jobPosition"
+          name="jobPosition"
+          value={jobPosition}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setJobPosition(event.target.value)
+          }
+        />
+      </label>
+      <label htmlFor="input_file">
+        <span>Image:</span>
+        <input
+          className={style.inputTypeFile}
+          type="file"
+          id="input_file"
+          name="file"
+          placeholder="Прикрепите файл"
+        />
+      </label>
+    </form>
+  );
+};
+
+export default Form;
--- pointing-poker/src/pages/main-page/MainPage.tsx
+++ pointing-poker/src/pages/main-page/MainPage.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import style from './Main-page.module.scss';
 
 import logo from '../../assets/images/Poker-Planning-logo.png';
+import Form from '../../components/form/Form';
 
 const MainPage: React.FC = (): JSX.Element => (
   <div className={style.main}>
     <h2 className={style.mainHeader}>MainPage</h2>
     <img className={style.mainLogo} src={logo} alt="logo" />
+    <Form />
   </div>
 );
 
