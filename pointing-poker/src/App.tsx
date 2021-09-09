0a4e400d0ae2eeab777b7dbd44be3e65c9b6212d
MrBarakD <79783082+MrBarakD@users.noreply.github.com>
2021-09-09 20:20:39 +0600

    Revert "feat: routing-animation (#2)"
    This reverts commit 0ed96abb924a49a4c4e35c980c420207a4a9b28b.

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -1,41 +1,30 @@
 import React from 'react';
-import { Switch, Route, useLocation } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { Switch, Route } from 'react-router-dom';
 
-import { Nav } from './components/header-nav/Nav';
+import style from './App.module.scss';
+
 import MainPage from './pages/main-page/MainPage';
+import NotFoundPage from './pages/not-found-page/NotFoundPage';
 import LobbyPage from './pages/lobby-page/LobbyPage';
 import GamePage from './pages/game-page/GamePage';
-import NotFoundPage from './pages/not-found-page/NotFoundPage';
 
-import Style from './App.module.scss';
-import cssTransition from './cssTransition.module.scss';
-
-const App: React.FC = (): JSX.Element => {
-  const location = useLocation();
-
-  return (
-    <>
-      <TransitionGroup>
-        <CSSTransition timeout={250} key={location.key} classNames={{ ...cssTransition }}>
-          <Switch location={location}>
-            <Route exact path="/">
-              <MainPage />
-            </Route>
-            <Route exact path="/lobby-page">
-              <LobbyPage />
-            </Route>
-            <Route path="/game-page">
-              <GamePage />
-            </Route>
-            <Route path="*">
-              <NotFoundPage />
-            </Route>
-          </Switch>
-        </CSSTransition>
-      </TransitionGroup>
-    </>
-  );
-};
+const App: React.FC = (): JSX.Element => (
+  <div className={style.app}>
+    <Switch>
+      <Route exact path="/">
+        <MainPage />
+      </Route>
+      <Route exact path="/lobby-page">
+        <LobbyPage />
+      </Route>
+      <Route path="/game-page">
+        <GamePage />
+      </Route>
+      <Route path="*">
+        <NotFoundPage />
+      </Route>
+    </Switch>
+  </div>
+);
 
 export default App;
