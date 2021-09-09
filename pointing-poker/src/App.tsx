0ed96abb924a49a4c4e35c980c420207a4a9b28b
MrBarakD <79783082+MrBarakD@users.noreply.github.com>
2021-09-09 18:48:36 +0600

    feat: routing-animation (#2)
    * merge: merge from development branch
    
    * feat: add css animation for routing, cauld be a bug with 404 and div elements
    
    * feat: add 404 link to test notFound page
    
    * refactor: change code for less conflicts during merge
    
    Co-authored-by: Daulet Barakov <dake.bakeuly@gmail.com>

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -1,30 +1,41 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
-import style from './App.module.scss';
-
+import { Nav } from './components/header-nav/Nav';
 import MainPage from './pages/main-page/MainPage';
-import NotFoundPage from './pages/not-found-page/NotFoundPage';
 import LobbyPage from './pages/lobby-page/LobbyPage';
 import GamePage from './pages/game-page/GamePage';
+import NotFoundPage from './pages/not-found-page/NotFoundPage';
 
-const App: React.FC = (): JSX.Element => (
-  <div className={style.app}>
-    <Switch>
-      <Route exact path="/">
-        <MainPage />
-      </Route>
-      <Route exact path="/lobby-page">
-        <LobbyPage />
-      </Route>
-      <Route path="/game-page">
-        <GamePage />
-      </Route>
-      <Route path="*">
-        <NotFoundPage />
-      </Route>
-    </Switch>
-  </div>
-);
+import Style from './App.module.scss';
+import cssTransition from './cssTransition.module.scss';
+
+const App: React.FC = (): JSX.Element => {
+  const location = useLocation();
+
+  return (
+    <>
+      <TransitionGroup>
+        <CSSTransition timeout={250} key={location.key} classNames={{ ...cssTransition }}>
+          <Switch location={location}>
+            <Route exact path="/">
+              <MainPage />
+            </Route>
+            <Route exact path="/lobby-page">
+              <LobbyPage />
+            </Route>
+            <Route path="/game-page">
+              <GamePage />
+            </Route>
+            <Route path="*">
+              <NotFoundPage />
+            </Route>
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    </>
+  );
+};
 
 export default App;
