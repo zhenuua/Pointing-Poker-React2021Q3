4dd6d54cd3ea223a40156fc7ee0ad863b5c09ce9
MrBarakD <79783082+MrBarakD@users.noreply.github.com>
2021-09-15 19:08:00 +0600

    Revert "Add Card" (#10)

--- pointing-poker/src/components/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import style from './Footer.module.scss';
-
-const Footer: React.FC = (): JSX.Element => {
-  return <footer className={style.footer} />;
-};
-
-export default Footer;
--- pointing-poker/src/react-app-env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react-scripts" />
-declare module 'react-double-marquee';
-declare module 'react-outside-click-handler';
