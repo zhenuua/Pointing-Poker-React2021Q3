dd6dd6deeaab22e7d62dff044c7cc84d44a498b3
Maxim Kalevich <maxkalevich@gmail.com>
2021-10-08 01:09:31 +0300

    feat: add new functionality

--- pointing-poker/src/components/statistics/Statistics/Statistics.tsx
+++ pointing-poker/src/components/statistics/Statistics/Statistics.tsx
@@ -15,15 +15,58 @@ const Statistics: React.FC<TStatistics> = ({ curScoreIndex }): JSX.Element => {
   );
   const { shortScoreType } = gameSettings;
 
+  // useEffect(() => {
+  //   players.length &&
+  //     players[0].scores.forEach((item) => {
+  //       const titleObject = {
+  //         issueTitle: item.issueTitle,
+  //         scores: [],
+  //       };
+  //       setValueArray(valueArray.push(titleObject));
+  //     });
+  // }, [players]);
+
   useEffect(() => {
-    players.length &&
-      players[0].scores.forEach((item) => {
-        const titleObject = {
-          issueTitle: item.issueTitle,
+    if (players.length) {
+      const arr: any = [];
+      players[0].scores.forEach((issue) => {
+        const obj = {
+          issueTitle: issue.issueTitle,
+          totalScores: [],
           scores: [],
         };
-        setValueArray(valueArray.push(titleObject));
+        arr.push(obj);
       });
+
+      players.forEach((player) => {
+        player.scores.forEach((score) => {
+          const obj = arr.find((issue: any) => issue.issueTitle === score.issueTitle);
+          if (obj) obj.totalScores.push(score.score);
+        });
+      });
+
+      if (arr.length) {
+        arr.forEach((issue: any) => {
+          const { totalScores, scores } = issue;
+          scores.sort((a: number, b: number) => a - b);
+          const newScore: any = [];
+          if (scores.score !== null)
+            while (totalScores.length) {
+              const count =
+                totalScores.lastIndexOf(totalScores[0]) -
+                totalScores.indexOf(totalScores[0]) +
+                1;
+              newScore.push({
+                value: totalScores[0],
+                count,
+              });
+
+              totalScores.splice(0, count);
+            }
+          scores.push(newScore);
+        });
+      }
+    }
   }, [players]);
 
   return (
