fc436f938f38162fa3fa3fe5e016b98046931aa4
Daulet Barakov <dake.bakeuly@gmail.com>
2021-10-09 10:44:47 +0600

    feat: add script for server for deploy

---
+++ pointing-poker/src/url-config/urls.ts
@@ -0,0 +1,5 @@
+export const SERVER_URL = 'http://localhost:5000';
+// export const SERVER_URL = 'http://localhost:5000';
+
+export const FRONT_URL = 'http://localhost:3000';
+// export const FRONT_URL = 'http://localhost:3000';
