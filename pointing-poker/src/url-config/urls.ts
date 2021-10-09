c22d46012dba63bef39d31fe0e83340d29cf66bf
zhenya zakharenko <zhenuua@gmail.com>
2021-10-09 13:13:57 +0300

    fix: update build

--- pointing-poker/src/url-config/urls.ts
+++ pointing-poker/src/url-config/urls.ts
@@ -1,5 +1,5 @@
 export const SERVER_URL = 'https://stark-savannah-25558.herokuapp.com';
 // export const SERVER_URL = 'http://localhost:5000';
 
-// export const FRONT_URL = 'https://pointing-poker-rss.netlify.app';
-export const FRONT_URL = 'http://localhost:3000';
+export const FRONT_URL = 'https://pointing-poker-rss.netlify.app';
+// export const FRONT_URL = 'http://localhost:3000';
